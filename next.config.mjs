f198d12d0f688738bf030e97c21fb841ad8b0a69
verdehile <hasminosmin@gmail.com>
2025-11-12 22:18:23 +0400

    fix: retry deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static HTML export
+  // basePath: "/preview-verdehile.github.io",
   typescript: {
     ignoreBuildErrors: true,
   },
